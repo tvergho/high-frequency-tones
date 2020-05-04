9833c6de9720b324572633f47ddab9d24e54b2cb
tvergho <tvergho@gmail.com>
2020-05-03 23:57:12 -0700

    It's working

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  resolver: {
+    /* resolver options */
+    sourceExts: ['jsx', 'js'], // add here
+  },
+  transformer: {
+    /* transformer options */
+  },
+  serializer: {
+    /* serializer options */
+  },
+  server: {
+    /* server options */
+  },
+
+  /* general options */
+};
